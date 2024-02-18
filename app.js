1fedb9d8552f449ac260a98d3f699530baa3fe83
Kerem Duran <kerem@weareoutpost.ca>
2024-02-17 19:10:23 -0500

    - server timeout removed (could be faulty)

--- app.js
+++ app.js
@@ -129,5 +129,5 @@ app.post("/content/:id",
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+server.keepAliveTimeout = 0;
+server.headersTimeout = 0;
